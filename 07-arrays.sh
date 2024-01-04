a5140754d63b5032f4b435a2b29afe731221ea1c
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-01-04 18:45:17 +0530

    added 2nd script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 FRUITS=("Apple" "Banana" "Mango")
 
-echo "First value:${FRUITS(0)}"
+echo "First value: ${FRUITS(0)}"
 
-echo "Second value:${FRUITS(1)}"
+echo "Second value: ${FRUITS(1)}"
 
-echo "Third value:${FRUITS(2)}"
+echo "Second value: ${FRUITS(2)}"
