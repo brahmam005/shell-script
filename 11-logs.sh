ecc5af8ffd23425d9ec91c6a91a50835de6ba995
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-01-05 17:03:17 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -26,7 +26,7 @@ else
    echo "You are root user"
 fi # fi means reverse of if, indicating condiction end
 
-yum install mysql -y &>> $LOGFILE
+yum install mysqll -y &>> $LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
